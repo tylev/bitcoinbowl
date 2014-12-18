2ff9380d0a4d0558761bc04b79bd02e0a2c4c8e8
Tyler Evans <tylermatthewevans@gmail.com>
2014-12-17 23:18:41 -0600

    desktop and mobile?

--- map.js
+++ map.js
@@ -409,3 +409,10 @@ $(function() {
     }, 10);
     addMap($('#map'), 6, 27.6648274, -81.5157535, markersJSON, [{}], myScroll, page3Scroll);
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-57790205-1', 'auto');
+  ga('send', 'pageview');
